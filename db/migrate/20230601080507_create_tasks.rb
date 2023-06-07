a906d6cf183d9b633448b1876cf2da32dde0f785
matheus <matheusiwanaga@gmail.com>
2023-06-07 20:00:57 +0900

    Before try_byebug

--- db/migrate/20230601080507_create_tasks.rb
+++ db/migrate/20230601080507_create_tasks.rb
@@ -1,10 +1,11 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :name
+      t.string :name, limit: 30, null: false
       t.text :description
 
       t.timestamps
+      t.index :name, unique: true
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_01_080507) do
+ActiveRecord::Schema.define(version: 2023_06_06_061515) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
